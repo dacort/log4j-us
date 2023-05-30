50666b2bd81a87f9766cce00f145294ea563eb7f
Damon P. Cortesi <d.lifehacker@gmail.com>
2023-05-29 22:21:27 -0700

    Move to fly.io, condense some code

--- dynalog.go
+++ dynalog.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"embed"
 	"fmt"
+	"io/fs"
 	"net/http"
 	"strings"
 	"text/template"
@@ -11,8 +13,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//go:embed templates/*
+var resources embed.FS
+
+//go:embed public/*
+var staticFS embed.FS
+
+var funcMap = template.FuncMap{"ToUpper": strings.ToUpper}
+
+func renderTemplate(c *gin.Context, templateName string) {
+	ll := buildLogLevelsFromRequestParams(c)
+
+	templatePath := fmt.Sprintf("templates/%s", templateName)
+	t, err := template.New(templateName).Funcs(funcMap).ParseFS(resources, templatePath)
+	if err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("template not found"))
+		return
+	}
+
+	var result bytes.Buffer
+	t.Execute(&result, ll)
+	c.String(http.StatusOK, result.String())
+}
+
 func main() {
-	funcMap := template.FuncMap{"ToUpper": strings.ToUpper}
+	
 	r := gin.Default()
 
 	// ForceSSL in production
@@ -22,36 +47,52 @@ func main() {
 	}))
 
 	// Basic site placeholder
-	r.Use(static.Serve("/", static.LocalFile("./public", true)))
+	r.Use(static.Serve("/", EmbedFolder(staticFS, "public", true)))
 
 	// Dynamic log builder
 	r.GET("/templates/:name", func(c *gin.Context) {
 		n := c.Param("name")
-		lp := buildLogProperties(c)
-		logFile, err := buildLog4j(n, lp)
-		if err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("bad request: %s", err))
-		}
-		c.String(http.StatusOK, string(logFile))
+		templateName := fmt.Sprintf("%s.log4j.properties", n)
+		renderTemplate(c, templateName)
 	})
 
 	// Now supporting log4jv2
 	r.GET("/v2/templates/:name", func(c *gin.Context) {
 		n := c.Param("name")
-		ll := buildLogLevelsFromRequestParams(c)
-
 		templateName := fmt.Sprintf("%s.log4j.xml", n)
-		t, err := template.New(templateName).Funcs(funcMap).ParseFiles("./templates/" + templateName)
-		if err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("template not found"))
-			return
-		}
-
-		var result bytes.Buffer
-		t.Execute(&result, ll)
-		c.String(http.StatusOK, result.String())
-
+		renderTemplate(c, templateName)
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+type embedFileSystem struct {
+	http.FileSystem
+	indexes bool
+}
+
+func (e embedFileSystem) Exists(prefix string, path string) bool {
+	f, err := e.Open(path)
+	if err != nil {
+		return false
+	}
+
+	// check if indexing is allowed
+	s, _ := f.Stat()
+	if s.IsDir() && !e.indexes {
+		return false
+	}
+
+	return true
+}
+
+func EmbedFolder(fsEmbed embed.FS, targetPath string, index bool) static.ServeFileSystem {
+	subFS, err := fs.Sub(fsEmbed, targetPath)
+	if err != nil {
+		panic(err)
+	}
+	return embedFileSystem{
+		FileSystem: http.FS(subFS),
+		indexes:    index,
+	}
 }
